e5240ad8cbf92dadec66b9b1b46eb5287eeb7d42
Andrew Duong <duongandrews@gmail.com>
2019-09-02 21:02:58 +0000

    Added hello_world.ejs and urls_index.ejs files

--- express_server.js
+++ express_server.js
@@ -3,12 +3,13 @@ const app = express();
 const PORT = 8080; // default ports 8080
 
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,9 +20,21 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = {
+    greeting: "Hello World!"
+  };
+  res.render("hello_world", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
